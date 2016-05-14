c5529f12b512cc29f0c115379b21347379e1336b
vanhung <vanhungbkcbg1@gmail.com>
2016-05-14 10:36:40 +0700

    fsd

--- index.php
+++ index.php
@@ -17,3 +17,8 @@ $test->doSomething();
 
 $db=new \Config\MyDB();
 $db->myfunction();
+
+//demo twitter connect
+$tw=new Twitter("xJmK06x5ZrMAWQB9tATHLW15F","KdeuIwErcp1I0se3fHBBqlGCbdQH8KmM6ZnY3Lmrz87hvFoLTz","730571895565737984-y2SH4f6LYRV4eamUntS0Qj5GrfsyQYo","FI4y8JWgnbOoHE6Pm9OlWHzOUxVw4Q8cj5ZoZnblbqFjO");
+$data=$tw->load(Twitter::ME);
+print_r($data[0]->user);
